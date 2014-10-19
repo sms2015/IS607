c2cd323425e8745602fc8ade4e0e5d51a2ebdf7e
sms2015 <transitplanner1@gmail.com>
2014-10-19 10:59:45 -0400

    Update IS607 Quiz 8.sql

--- IS607/IS607 Quiz 8.sql
+++ IS607/IS607 Quiz 8.sql
@@ -1,19 +1,26 @@
 --Stacey Schwarcz
 --Week 8 Quiz
---Week 8 quiz is due end of day on Sunday October 19th. A solution will be posted on Monday October 20th.
 --For this week’s quiz, you’re asked to store information about an organizational hierarchy in a relational database
 --(PostgreSQL).
+
 --Here is an (obviously not current) org chart for Apple Corporation:
 --http://fortunedotcom.files.wordpress.com/2011/08/apple_org_chart_large1.jpg
---(1) Create a single table that includes information for the CEO, the SVP of IOS Software, and the SVP, Chief 
---Financial Officer. Also include information for the direct reports of the two SVPs. [You should have a single 
---table with 8 rows].
---(2) Write a query that displays all of the information in the table.
---(3) Assume that (a) Tim Cook replaces Steve Jobs as CEO, and (b) Apple (hypothetically) hires Susan Wojcicki
-away from Google to replace Tim Cook at COO, with the COO reports unchanged.
-Your deliverable should include:
- The script to create the table,
- The INSERT statements to populate the table, and 
- The SQL query to pull information from the table, and
- The [INSERT, UPDATE, DELETE] SQL statements needed to update the information to reflect the new CEO 
-and COO as described in (3) above.
+
+--(1)--
+--Create a single table that includes information for the CEO, the SVP of IOS Software, and the SVP, Chief Financial Officer. 
+--Also include information for the direct reports of the two SVPs. [You should have a single table with 8 rows].
+
+--(2)--
+--Write a query that displays all of the information in the table.
+
+
+--(3)--
+--Assume that (a) Tim Cook replaces Steve Jobs as CEO, and (b) Apple (hypothetically) hires Susan Wojcicki
+--away from Google to replace Tim Cook at COO, with the COO reports unchanged.
+
+--Your deliverable should include:
+--The script to create the table,
+--The INSERT statements to populate the table, and 
+--The SQL query to pull information from the table, and
+--The [INSERT, UPDATE, DELETE] SQL statements needed to update the information to reflect the new CEO and COO 
+--as described in (3) above.
