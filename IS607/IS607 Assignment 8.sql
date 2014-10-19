5d0e08367166a76babb70f6de59f2a8a88bc0c6c
sms2015 <transitplanner1@gmail.com>
2014-10-19 16:23:21 -0400

    Update IS607 Assignment 8.sql

--- IS607/IS607 Assignment 8.sql
+++ IS607/IS607 Assignment 8.sql
@@ -4,20 +4,30 @@
 --You’re asked to design a small database that tracks blog posts, comments, and tags. You should:
 
 --1.--
---Perform an Environmental Scan. Identify a favorite blog that has comments and tags enabled. Spend a 
---few minutes looking at the site, and think about how posts, comments, and tags are related. Deliverable: 
---URL of a favorite blog (or other type of site) that implements posts, comments, and tags.
+--Perform an Environmental Scan
+--Identify a favorite blog that has comments and tags enabled. Spend a few minutes looking at the site, 
+--and think about how posts, comments, and tags are related. 
+--Deliverable: URL of a favorite blog (or other type of site) that implements posts, comments, and tags.
+
+--Answer:
+--Url: http://slice.mit.edu/
 
 --2.--
 --Design the Logical Database. Determine what are the entitities (tables) and relationships between these 
 --entities (1:1, 1:M, M:M?). Determine what are the most important attributes (columns) for each entity. It is 
 --fine to make simplifying assumptions here—for example, we don’t need to track multiple authors, or 
 --votes/ratings on posts. [Please feel free to implement any of these after you’ve got the core assignment 
---working]. Deliverable: an entity-relationship diagram (hand-drawn is acceptable).
+--working]. 
+--Deliverable: an entity-relationship diagram (hand-drawn is acceptable).
+
+--Answer:
+--See attached pdf entitiy-relationship diagram
 
 --3. --
---Implement the Physical Database. Implement the database that you designed in step 1. Here, your 
---deliverable is the script that creates the tables.
+--Implement the Physical Database. 
+--Implement the database that you designed in step 1. 
+--Here, your deliverable is the script that creates the tables.
+
 
 --4.--
 --Populate the Physical Database with Sample Data. Write INSERT statements that add a few rows to 
