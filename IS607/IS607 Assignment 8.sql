aa79122bd435020de2c2c405011ec59ae30b6024
sms2015 <transitplanner1@gmail.com>
2014-10-19 13:37:33 -0400

    Create IS607 Assignment 8.sql

---
+++ IS607/IS607 Assignment 8.sql
@@ -0,0 +1,29 @@
+--Stacey Schwarcz
+--Week 8 assignment
+
+--You’re asked to design a small database that tracks blog posts, comments, and tags. You should:
+
+--1.--
+--Perform an Environmental Scan. Identify a favorite blog that has comments and tags enabled. Spend a 
+--few minutes looking at the site, and think about how posts, comments, and tags are related. Deliverable: 
+--URL of a favorite blog (or other type of site) that implements posts, comments, and tags.
+
+--2.--
+--Design the Logical Database. Determine what are the entitities (tables) and relationships between these 
+--entities (1:1, 1:M, M:M?). Determine what are the most important attributes (columns) for each entity. It is 
+--fine to make simplifying assumptions here—for example, we don’t need to track multiple authors, or 
+--votes/ratings on posts. [Please feel free to implement any of these after you’ve got the core assignment 
+--working]. Deliverable: an entity-relationship diagram (hand-drawn is acceptable).
+
+--3. --
+--Implement the Physical Database. Implement the database that you designed in step 1. Here, your 
+--deliverable is the script that creates the tables.
+
+--4.--
+--Populate the Physical Database with Sample Data. Write INSERT statements that add a few rows to 
+--each of the tables, so that the appropriate (one-to-one and one-to-many) relationships can be tested. 
+--Deliverable: a single script with all of the insert statements.
+
+--5. --
+--Query the Data. Here, your deliverables are to write (1) a SQL query that returns all of the blog posts, with 
+--associated comments and tags; (2) a SQL query that returns all of the posts for a given tag.
